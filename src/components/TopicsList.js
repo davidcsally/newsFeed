774628f1bed18c004f2d1ab8b56516d44eca9fd0
David Sally <davidcsally@gmail.com>
2017-10-25 15:56:52 -0700

    refactored to stateless, linted

--- src/components/TopicsList.js
+++ src/components/TopicsList.js
@@ -2,36 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { requestApiTopics } from '../reducers/actions';
 import TopicItem from './TopicItem';
 
 /** TopicList.js
  *
  * Container for Topic items. This list is subscribed to 
  */
-class TopicsList extends Component {
-  constructor() {
-    super();
-  }
 
-  render() {
-    return (
-      <div className="topics-list">
-        <h4>Topics</h4>
-        {this.props.topics.map(topic => (
-          <TopicItem key={topic.id} {...topic} />
-        ))}
-      </div>
-    );
-  }
-}
+const TopicsList = props => (
+  <div className="topics-list">
+    <h4>Topics</h4>
+    {props.topics.map(topic => (
+      <TopicItem key={topic.id} {...topic} />
+    ))}
+  </div>
+);
 
 TopicsList.propTypes = {
-  topics: PropTypes.array.isRequired,
-  requestApiTopics: PropTypes.func.isRequired,
+  topics: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
 };
 
-export default connect(
-  state => ({ topics: state.topics }),
-  { requestApiTopics },
-)(TopicsList);
+export default connect(state => ({ topics: state.topics }))(TopicsList);
