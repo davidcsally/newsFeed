c31b5477896085f07866756f77ec250ce60b63a9
David Sally <davidcsally@gmail.com>
2017-10-24 18:31:42 -0700

    refactored sagas

--- src/sagas/asyncSagas.js
+++ src/sagas/asyncSagas.js
@@ -1,21 +1,39 @@
 // import { takeLatest } from 'redux-saga/effects';
 import { call, put, takeLatest } from 'redux-saga/effects';
 
-import { REQUEST_API_DATA, receiveApiData } from '../reducers/actions';
-import { fetchData } from "./API";
+import { REQUEST_API_ARTICLES, receiveApiArticles, REQUEST_API_TOPICS, recieveApiTopics, followAllTopics } from '../reducers/actions';
+import { fetchArticles, fetchTopics } from './API';
 
 
-function* getApiData(action) {
+function* getApiArticles(action) {
   try {
     // do api call
-    const data = yield call(fetchData);
-    yield put(receiveApiData(data));
+    const data = yield call(fetchArticles);
+    yield put(receiveApiArticles(data));
   }
   catch (e) {
     console.log('error: ', e);
   }
 }
 
+function* getApiTopics(action) {
+  console.log('# getAPITopics');
+  let initialFetch = true;
+  try {
+    const data = yield call(fetchTopics);
+    yield put(recieveApiTopics(data));
+
+    if (initialFetch === true) {
+      initialFetch = false;
+      yield put(followAllTopics(data));
+    }
+  }
+  catch (e) {
+    console.log('Error: ', e);
+  }
+}
+
 export default function* mySaga() {
-  yield takeLatest(REQUEST_API_DATA, getApiData);
+  yield takeLatest(REQUEST_API_ARTICLES, getApiArticles);
+  yield takeLatest(REQUEST_API_TOPICS, getApiTopics);
 }
