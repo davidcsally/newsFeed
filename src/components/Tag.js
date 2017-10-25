b5a01944c09f5369ee398f1c628a561ddbf65921
David Sally <davidcsally@gmail.com>
2017-10-25 16:03:29 -0700

    linted

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Tag = (props) => {
-  return (
-    <div className="tag-item">
-      {props.name}
-    </div>
-  );
+/**
+ * Tag.js
+ *
+ * List items corresponding to article categories
+ * Props:
+ *  @param {String} name - Category name
+ */
+const Tag = props => (
+  <div className="tag-item">
+    {props.name}
+  </div>
+);
+
+Tag.propTypes = {
+  name: PropTypes.string.isRequired,
 };
 
 export default Tag;
