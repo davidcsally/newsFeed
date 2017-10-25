ebbc1b8cf8f96e5b7c2159f41d90e192e337e78c
David Sally <davidcsally@gmail.com>
2017-10-25 16:09:00 -0700

    refactored to stateless component, linted

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -5,43 +5,35 @@ import PropTypes from 'prop-types';
 import { requestApiArticles } from '../reducers/actions';
 import NewsItem from './NewsItem';
 
-class ArticlesList extends Component {
-  constructor() {
-    super();
-  }
+const ArticlesList = (props) => {
+  const followedTopics = [];
+  const keys = Object.keys(props.topics);
+  keys.forEach((key) => {
+    const topic = props.topics[key];
+    if (topic.isFollowing) followedTopics.push(topic.name);
+  });
 
-  render() {
-    const followedTopics = [];
-    const keys = Object.keys(this.props.topics);
-    keys.forEach((key) => {
-      const topic = this.props.topics[key];
-      if (topic.isFollowing) followedTopics.push(topic.name);
-    });
-
-    return (
-      <div className="articles-list">
-        {this.props.articles.map((article) => {
-          for (let i = 0; i < article.topics.length; i += 1) {
-            if (followedTopics.includes(article.topics[i].name)) {
-              return <NewsItem key={article.id} {...article} />;
-            }
-          }
-        })
+  const followedArticles = props.articles.map((article) => {
+    for (let i = 0; i < article.topics.length; i += 1) {
+      if (followedTopics.includes(article.topics[i].name)) {
+        return <NewsItem key={article.id} {...article} />;
       }
-      </div>
-    );
-  }
-}
+    }
+  });
+
+  return (
+    <div className="articles-list">
+      {followedArticles}
+    </div>
+  );
+};
 
 ArticlesList.propTypes = {
   articles: PropTypes.array.isRequired,
-  topics: PropTypes.array.isRequired,
-  requestApiArticles: PropTypes.func.isRequired,
+  topics: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
 };
 
-export default connect(
-  state => ({ articles: state.articles, topics: state.topics }),
-  { requestApiArticles },
-)(ArticlesList);
-
-// <NewsItem key={article.id} {...article} />)}
+export default connect(state => ({ articles: state.articles, topics: state.topics }))(ArticlesList);
