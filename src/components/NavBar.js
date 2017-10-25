074f41b358f7729ebf6fccdfb9d52aefcac8a085
David Sally <davidcsally@gmail.com>
2017-10-25 15:22:09 -0700

    refactored

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
@@ -7,11 +7,7 @@ const NavBar = () => (
       {/* eslint-disable */}
       <button type="button" className="btn btn-primary"><Link to="/">Home</Link></button>
       <button type="button" className="btn btn-primary"><Link to="/topics">Topics</Link></button>
-      {/* <li><Link to="/">Home</Link></li> */}
       <li>Articles Feed</li>
-      {/* <li><Link to="/topics">Topics</Link></li> */}
-
-
       {/* eslint-enable */}
     </ul>
   </nav>
