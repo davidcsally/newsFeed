4d4512d2de2b44e18873c1cd101c268f92315af9
David Sally <davidcsally@gmail.com>
2017-10-25 00:21:43 -0700

    consistent buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-// class NavBar extends Component {
-//   constructor() {
-//     super();
-//   }
-
-//   render() {
-
-//   }
-// }
 const NavBar = () => (
   <nav>
-    <ul className="nav-list">
+    <ul className="navbar">
       {/* eslint-disable */}
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/topics">Topics</Link></li>
+      <button type="button" className="btn btn-primary"><Link to="/">Home</Link></button>
+      <button type="button" className="btn btn-primary"><Link to="/topics">Topics</Link></button>
+      {/* <li><Link to="/">Home</Link></li> */}
+      <li>Articles Feed</li>
+      {/* <li><Link to="/topics">Topics</Link></li> */}
+
+
       {/* eslint-enable */}
     </ul>
   </nav>
