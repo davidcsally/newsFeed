3dba39618484796f826b5988893feded7fc9802b
David Sally <davidcsally@gmail.com>
2017-10-24 18:33:01 -0700

    refactored, removed API call

---
+++ src/components/TopicsList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { requestApiTopics } from '../reducers/actions';
+import TopicItem from './TopicItem';
+
+class TopicsList extends Component {
+  constructor() {
+    super();
+    console.log('# TopicsList Loading');
+  }
+
+  render() {
+    return (
+      <div className="topics-list">
+        Topics list
+        {this.props.topics.map(topic => (
+          <TopicItem key={topic.id} {...topic} />
+        ))}
+      </div>
+    );
+  }
+}
+
+TopicsList.propTypes = {
+  topics: PropTypes.array.isRequired,
+  requestApiTopics: PropTypes.func.isRequired,
+};
+
+export default connect(
+  state => ({ topics: state.topics, following: state.following }),
+  { requestApiTopics },
+)(TopicsList);
