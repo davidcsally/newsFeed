b5071265c40f26fb5c34cbb6ff1b3520d1c3174b
David Sally <davidcsally@gmail.com>
2017-10-25 15:16:20 -0700

    minor refactor, added comments

--- src/components/TopicItem.js
+++ src/components/TopicItem.js
@@ -4,19 +4,43 @@ import { connect } from 'react-redux';
 
 import { followTopic, unfollowTopic } from '../reducers/actions';
 
+/**
+ * TopicItem.js
+ *
+ * Each Topic Item represents one topic that the user can follow.
+ * Topic items are subscribed to an array of followed topics
+ * Props:
+ *  @param {String} name - Topic Name
+ *  @param {Bool} isFollowing - Flag if user is / is not following
+ *  @param {Func} followTopic - Dispatch event to follow topic
+ *  @param {Func} unfollowTopic - Dispatch event to unfollow topic
+ *
+ */
 const TopicItem = (props) => {
+  // If following, show unfollow button
   if (props.isFollowing) {
     return (
       <div className="topic-item">
         {props.name}
-        <button type="button" className="btn btn-primary"  onClick={() => props.unfollowTopic(props)}>✅ Following</button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => props.unfollowTopic(props)}
+        >✓  Following
+        </button>
       </div>
     );
   }
+  // If not following, show follow button
   return (
     <div className="topic-item">
       {props.name}
-      <button type="button" className="btn btn-primary" onClick={() => props.followTopic(props)}>+ Follow</button>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => props.followTopic(props)}
+      >+ Follow
+      </button>
     </div>
   );
 };
@@ -24,6 +48,8 @@ const TopicItem = (props) => {
 TopicItem.propTypes = {
   name: PropTypes.string.isRequired,
   isFollowing: PropTypes.bool.isRequired,
+  followTopic: PropTypes.func.isRequired,
+  unfollowTopic: PropTypes.func.isRequired,
 };
 
 export default connect(
