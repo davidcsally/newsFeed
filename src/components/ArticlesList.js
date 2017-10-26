ea55cd303cde856510fd9780ab2f70026421fec9
David Sally <davidcsally@gmail.com>
2017-10-25 22:46:15 -0700

    refactored filters into higher order functions

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -2,28 +2,45 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { requestApiArticles } from '../reducers/actions';
 import NewsItem from './NewsItem';
 
 const ArticlesList = (props) => {
-  const followedTopics = [];
-  const keys = Object.keys(props.topics);
-  keys.forEach((key) => {
-    const topic = props.topics[key];
-    if (topic.isFollowing) followedTopics.push(topic.name);
-  });
+  // extract followed topics
+  const values = Object.values(props.topics);
+  const followedTopics = values
+    .map((val) => {
+      if (val.isFollowing === true) return val.name;
+    }) // remove holes in array (left by map)
+    .filter((elem) => {
+      if (elem) return elem;
+    });
 
-  const followedArticles = props.articles.map((article) => {
-    for (let i = 0; i < article.topics.length; i += 1) {
-      if (followedTopics.includes(article.topics[i].name)) {
-        return <NewsItem key={article.id} {...article} />;
+  // build list of articles to display, by filtering through followed topics
+  const followedArticles = props.articles
+    .map((article) => {
+      for (let i = 0; i < article.topics.length; i += 1) {
+        if (followedTopics.includes(article.topics[i].name)) {
+          return <NewsItem key={article.id} {...article} />;
+        }
       }
-    }
-  });
+    }) // remove holes in array (left by map)
+    .filter((elem) => {
+      if (elem) return elem;
+    });
 
+  // if there are articles available, display articles
+  if (followedArticles.length > 0) {
+    return (
+      <div className="articles-list">
+        {followedArticles}
+      </div>
+    );
+  }
+
+  // if no articles, inform user to follow more topics
   return (
     <div className="articles-list">
-      {followedArticles}
+      Try following some more topics!
     </div>
   );
 };
