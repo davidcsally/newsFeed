4ef02ffb652739bb84caf759aa00b781d93b9fec
David Sally <davidcsally@gmail.com>
2017-10-25 20:57:24 -0700

    minor clean up

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+/* eslint-disable */ // shhh eslint, webpack is bundling this
 import styles from './css/style.css';
+/* eslint-enable */
 
 ReactDOM.render(<App />, document.getElementById('root'));
