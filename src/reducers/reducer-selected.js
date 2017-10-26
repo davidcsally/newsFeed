59f4074878c96a6a28e068aa02c22f9d02846d3f
David Sally <davidcsally@gmail.com>
2017-10-25 20:44:55 -0700

    documented

--- src/reducers/reducer-selected.js
+++ src/reducers/reducer-selected.js
@@ -1,14 +1,22 @@
 import { RECIEVE_SINGLE_ARTICLE, CLEAR_SELECTED_ARTICLE } from './actions';
 
+/**
+ *  Selected Article Reducer
+ *
+ * -Recieve article (from GET)
+ * -Clear article (so that wrong article is never shown)
+ */
 export default (state = {}, { type, data }) => {
   switch (type) {
     case RECIEVE_SINGLE_ARTICLE: {
       const newData = data.data[0];
       return { ...newData };
     }
+
     case CLEAR_SELECTED_ARTICLE: {
       return {};
     }
+
     default: {
       return state;
     }
